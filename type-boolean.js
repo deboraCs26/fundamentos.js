8096688ffab404ba40a826643bc73db6a210ff55
debora <debora.sesv@gmail.com>
2022-12-18 15:58:15 -0300

    exemplos de tipos primitivos

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 10;
+const segundoNumero = 10;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Débora";
+const texto2 = "Carvalho";
+
+console.log(texto1 === texto2)
---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number 
+
+const meuNumero = 6;
+
+const primeiroNumero = 5;
+const segundoNumero = 2;
+
+const operacaoMAtematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMAtematica)
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not Number (não é um numero)
+
+const alura = 'Alura';
+console.log(alura * primeiroNumero)
