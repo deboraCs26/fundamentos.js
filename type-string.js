75844df9758ae69864c48f4ec20ecfaaa8787f9e
debora <debora.sesv@gmail.com>
2022-12-18 16:15:47 -0300

    tipo string

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Bem vindos!";
+const texto2 = "Pessoal!";
+const senha = "senhaSuporteSegurança!";
+const StringDeNumeros = "123456";
+
+const citacao = "Meu nome é ";
+const meuNome = "Débora";  
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal
